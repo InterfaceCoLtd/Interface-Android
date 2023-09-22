5ade857681042dd7fe61849952c4b1a8e2101cda
YeoungU <ywkim098@gmail.com>
2023-09-22 09:30:35 +0900

    activity_finish_signup 오타수정

--- app/src/main/java/com/example/interface_android/activity_finish_signup.kt
+++ app/src/main/java/com/example/interface_android/activity_finish_signup.kt
@@ -10,7 +10,7 @@ class activity_finish_signup : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish_signup)
         val bt_finish_signup: Button = findViewById(R.id.bt_finish_signup) //로그인하러가기 버튼을 누르면 로그인 페이지로 이동한다.
-        bt_finish_signup.setOnClickListener{
+        bt_finish_signup.setOnClickListener{ //화면전환
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
